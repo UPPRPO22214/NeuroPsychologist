645c5d0c6e2dca9f0214b322833c767aa224c791
Marya Yukhnina <m.yukhnina@g.nsu.ru>
2025-11-04 06:29:50 +0300

    KAN-14: fix health

--- neuro_psychologist_backend/src/main/java/ru/nsu/neuropsychologist/neuro_psychologist_backend/controller/HealthController.java
+++ neuro_psychologist_backend/src/main/java/ru/nsu/neuropsychologist/neuro_psychologist_backend/controller/HealthController.java
@@ -1,10 +1,16 @@
 package ru.nsu.neuropsychologist.neuro_psychologist_backend.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/health")
+@CrossOrigin(origins = "*")
 public class HealthController {
-    @GetMapping("/health")
+    @GetMapping("*")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("Сервис анализа работает");
     }
