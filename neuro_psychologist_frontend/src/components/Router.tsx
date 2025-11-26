b6ead6f1e508200ba445a8de0fce1fc0c4692b96
Marya Yukhnina <m.yukhnina@g.nsu.ru>
2025-11-26 19:57:02 +0300

    KAN-16: add navigation with react-router

---
+++ neuro_psychologist_frontend/src/components/Router.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WelcomePage from '../pages/welcome-page';
+import AuthPage from '../pages/authorization-page';
+import ChatPage from '../pages/chat-page';
+
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
