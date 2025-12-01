4c887f42a0313920d3a132854789b7b29ee16f01
Marya Yukhnina <m.yukhnina@g.nsu.ru>
2025-12-01 22:37:12 +0300

    KAN-25: setting day analysis endpoint

---
+++ neuro_psychologist_backend/src/main/java/ru/nsu/neuropsychologist/neuro_psychologist_backend/repository/DayAnalysisRepository.java
@@ -0,0 +1,23 @@
+package ru.nsu.neuropsychologist.neuro_psychologist_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.nsu.neuropsychologist.neuro_psychologist_backend.entity.DayAnalysis;
+import ru.nsu.neuropsychologist.neuro_psychologist_backend.entity.User;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Repository
+public interface DayAnalysisRepository extends JpaRepository<DayAnalysis, Long> {
+    
+    List<DayAnalysis> findByUserOrderByAnalyzedAtDesc(User user);
+    
+    List<DayAnalysis> findByUserAndAnalyzedAtBetweenOrderByAnalyzedAtDesc(
+        User user, 
+        ZonedDateTime startDate, 
+        ZonedDateTime endDate
+    );
+    
+    List<DayAnalysis> findTop10ByUserOrderByAnalyzedAtDesc(User user);
+}
